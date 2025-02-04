c10ecc4914e62d09838186232d888252f33dfe84
MohamedHassan1996 <mohamedhassan00096@gmail.com>
2025-02-04 16:38:18 +0200

    m

--- app/Http/Controllers/Api/Dashboard/Course/CoursePriceController.php
+++ app/Http/Controllers/Api/Dashboard/Course/CoursePriceController.php
@@ -43,7 +43,7 @@ class CoursePriceController extends Controller
 
         return response()->json([
             'data' => [
-                'coursePrice' => $coursePrice
+                'coursePrice' => $coursePrice->price
             ]
         ]);
 
